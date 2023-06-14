2b8052713dc98c1f12711b7b53e2837abb150eda
Naimul Hassan Noor <naimul.roni@gmail.com>
2023-06-14 17:50:47 +0600

    init opg

--- example/src/main/java/vtsen/hashnode/dev/buildutils/example/ui/theme/Shape.kt
+++
@@ -1,7 +0,0 @@
-package vtsen.hashnode.dev.buildutils.example.ui.theme
-
-import androidx.compose.material3.Shapes
-
-val Shapes = Shapes(
-    /* Override CornerBasedShape Here */
-)
--- example/src/main/java/vtsen/hashnode/dev/buildutils/example/ui/theme/Type.kt
+++
@@ -1,7 +0,0 @@
-package vtsen.hashnode.dev.buildutils.example.ui.theme
-
-import androidx.compose.material3.Typography
-
-val Typography = Typography(
-    /* Override TextStyle Here */
-)
